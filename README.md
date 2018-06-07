9e26b5c27ce146fbad033081398ee84d1d0ddf4b
Yuhang <abeyuhang@gmail.com>
2018-06-07 11:41:43 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# vscode-lesslint
+
+A [Visual Studio Code](https://code.visualstudio.com/) extension to lint **only** [Less](http://lesscss.org/) with [stylelint](https://stylelint.io/)
+
+### Optional (but recommended) setup
+
+To prevent both [the editor built-in linters](https://code.visualstudio.com/docs/languages/css#_syntax-verification-linting) `[less]` and this extension `[lesslint]` from reporting essentially the same errors like in the screenshot, disable the built-in ones in User or Workspace [setting](https://code.visualstudio.com/docs/getstarted/settings):
+
+```json
+"less.validate": false
+```
+
+## Extension Settings
+
+This extension has no settings!
+
+Just use `.stylelintrc` and `.stylelintignore` under project root directory. See [stylelint configuration file](https://stylelint.io/user-guide/example-config/). 
---
+++ test/lesslint.test.js
@@ -0,0 +1,30 @@
+/* global suite, test */
+
+const {
+  extensions,
+  workspace,
+  window
+} = require('vscode');
+const assert = require('assert');
+
+suite('vscode-lesslint', function () {
+  const vscodeLesslint = extensions.getExtension('yuhang.lesslint');    
+  assert.equal(!!vscodeLesslint, true, 'Lesslint extensition is not installed');
+
+  test('should not be activated when the open file is not less.', async function () {
+    const plaintextDocument = await workspace.openTextDocument({
+      content: 'Hello',
+      language: 'plaintext'
+    });
+    await window.showTextDocument(plaintextDocument);
+    assert.equal(vscodeLesslint.isActive, false);
+  });
+  test('should be activated when the open file is less.', async function() {
+    const lessDocument = await workspace.openTextDocument({
+      content: 'padding: 0;',
+      language: 'less'
+    });
+    await window.showTextDocument(lessDocument);
+    assert.equal(vscodeLesslint.isActive, true);
+  });
+});
